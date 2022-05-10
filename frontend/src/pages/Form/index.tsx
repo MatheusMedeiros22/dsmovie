513a11f07baf7dfa048a01a53d58656611a5624b
Matheus Medeiros <mathmed22@gmail.com>
2022-05-10 16:33:25 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,7 @@
+function Form(){
+    return (
+        <h1>Paginas de formulario</h1>
+    )
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+function Listing(){
+    return (
+        <h1>Paginas de listagem</h1>
+    )
+}
+
+export default Listing;
